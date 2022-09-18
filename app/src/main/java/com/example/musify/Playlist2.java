e622626103e99270a722e2adab4bbed9eaf7b85e
Hassaan Asif <hassaanasif1998@hotmail.com>
2022-09-18 22:36:51 +0500

    Initial commit

---
+++ app/src/main/java/com/example/musify/Playlist2.java
@@ -0,0 +1,14 @@
+package com.example.musify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Playlist2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playlist2);
+    }
+}
